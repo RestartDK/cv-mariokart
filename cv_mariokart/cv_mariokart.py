64a35cb7cb844870fa8a11eb38697b1556d6c320
R3call <thenoobyzombie@gmail.com>
2025-03-07 15:08:59 +0100

    chore: removed duplicate function

--- cv_mariokart/cv_mariokart.py
+++ cv_mariokart/cv_mariokart.py
@@ -1,19 +1,5 @@
-import os.path
 from .controller import Controller
-
-
-def find_dolphin_dir():
-    """Attempts to find the dolphin user directory. None on failure."""
-    candidates = [
-        "~/.dolphin-emu",
-        "~/.local/share/.dolphin-emu",
-        "~/Library/Application Support/Dolphin/",
-    ]
-    for candidate in candidates:
-        path = os.path.expanduser(candidate)
-        if os.path.isdir(path):
-            return path
-    return None
+from .utils import find_dolphin_dir
 
 
 def main():
